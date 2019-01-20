52ec1a6265b3d39e6b294b16eacffc39bed9c5c2
geneing <geneing@gmail.com>
2019-01-20 11:51:42 -0800

    Added pruning.

--- hparams.py
+++ hparams.py
@@ -3,83 +3,90 @@ import tensorflow as tf
 # Default hyperparameters:
 hparams = tf.contrib.training.HParams(
     name="WaveRNN",
-    num_workers = 8,
+    num_workers=8,
     # Input type:
     # 1. raw [-1, 1]
     # 2. mixture [-1, 1]
     # 3. bits [0, 512]
     # 4. mulaw[0, mulaw_quantize_channels]
     #
-    input_type = 'bits',
+    input_type='bits',
     #
     # distribution type, currently supports only 'beta' and 'mixture'
-    distribution = 'beta', # or "mixture"
-    log_scale_min = -32.23619130191664, # = float(np.log(1e-7))
-    quantize_channels = 65536, # quantize channel used for compute loss for mixture of logistics
+    distribution='beta',  # or "mixture"
+    log_scale_min=-32.23619130191664,  # = float(np.log(1e-7))
+    quantize_channels=65536,  # quantize channel used for compute loss for mixture of logistics
     #
     # for Fatcord's original 9 bit audio, specify the audio bit rate. Note this corresponds to network output
     # of size 2**bits, so 9 bits would be 512 output, etc.
-    bits = 10,
+    bits=10,
     # for mu-law
-    mulaw_quantize_channels = 512,
+    mulaw_quantize_channels=512,
     # note: r9r9's deepvoice3 preprocessing is used instead of Fatcord's original.
-    #--------------     
+    # --------------
     # audio processing parameters
-    num_mels = 80,
-    fmin = 95,
-    fmax = 7600,
-    n_fft = 2048,
-    hop_size = 200,
-    win_size = 800,
-    sample_rate = 16000,
-    preemphasis = 0.97,
-    min_level_db = -100,
-    ref_level_db = 20,
-    rescaling = False,
-    rescaling_max = 0.999,
-    allow_clipping_in_normalization = True,
-    use_lws = False,
-    #----------------
+    num_mels=80,
+    fmin=95,
+    fmax=7600,
+    n_fft=2048,
+    hop_size=200,
+    win_size=800,
+    sample_rate=16000,
+    preemphasis=0.97,
+    min_level_db=-100,
+    ref_level_db=20,
+    rescaling=False,
+    rescaling_max=0.999,
+    allow_clipping_in_normalization=True,
+    use_lws=False,
+    # ----------------
     #
-    #----------------
+    # ----------------
     # model parameters
-    rnn_dims = 512,
-    fc_dims = 512,
-    pad = 2,
+    rnn_dims=512,
+    fc_dims=512,
+    pad=2,
     # note upsample factors must multiply out to be equal to hop_size, so adjust
     # if necessary (i.e 4 x 5 x 10 = 200)
-    upsample_factors = (4, 5, 10),
-    compute_dims = 128,
-    res_out_dims = 128,
-    res_blocks = 10,
-    #----------------
+    upsample_factors=(4, 5, 10),
+    compute_dims=128,
+    res_out_dims=128,
+    res_blocks=10,
+    # ----------------
     #
-    #----------------
+    # ----------------
     # training parameters
-    batch_size = 64,
-    nepochs = 5000,
-    save_every_step = 10000,
-    evaluate_every_step = 5000,
+    batch_size=64,
+    nepochs=5000,
+    save_every_step=5000,
+    evaluate_every_step=5000,
     # seq_len_factor can be adjusted to increase training sequence length (will increase GPU usage)
-    seq_len_factor = 5,
+    seq_len_factor=5,
 
-    grad_norm = 10,
-    #learning rate parameters
+    grad_norm=10,
+    # learning rate parameters
     initial_learning_rate=1e-3,
-    lr_schedule_type = 'noam', # or 'noam'
+    lr_schedule_type='noam',  # or 'noam'
 
     # for step learning rate schedule
-    step_gamma = 0.5,
-    lr_step_interval = 15000,
+    step_gamma=0.5,
+    lr_step_interval=15000,
+
+    # sparsification
+    start_prune=20000,
+    prune_steps=20000,  # 20000
+    sparsity_target=0.9375,
+    sparse_group=4,
 
     adam_beta1=0.9,
     adam_beta2=0.999,
     adam_eps=1e-8,
     amsgrad=False,
     weight_decay=1e-5,
-    fix_learning_rate = None, # modify if one wants to use a fixed learning rate, else set to None to use noam learning rate
-    #-----------------
-    batch_size_gen = 32,
+    fix_learning_rate=None,
+    # modify if one wants to use a fixed learning rate, else set to None to use noam learning rate
+    # -----------------
+    batch_size_gen=32,
 )
 
 hparams.seq_len = hparams.seq_len_factor * hparams.hop_size
--- train.py
+++ train.py
@@ -8,40 +8,164 @@ options:
     --log-event-path=<path>     Path to tensorboard event log
     -h, --help                  Show this help message and exit
 """
-from docopt import docopt
-
 import os
-from os.path import dirname, join, expanduser
-from tqdm import tqdm
 
-from datetime import datetime
-import numpy as np
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import librosa
+# matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from datetime import datetime
+from docopt import docopt
+from os.path import join
 from tensorboardX import SummaryWriter
-
-from model import build_model
-
-import torch
 from torch import nn
-import torch.nn.functional as F
 from torch import optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-from model import build_model
-from distributions import *
-from loss_function import nll_loss
 from dataset import raw_collate, discrete_collate, AudiobookDataset
+from distributions import *
 from hparams import hparams as hp
+from loss_function import nll_loss
 from lrschedule import noam_learning_rate_decay, step_learning_rate_decay
+from model import build_model
 from utils import num_params_count
 
 global_step = 0
 global_epoch = 0
 global_test_step = 0
 use_cuda = torch.cuda.is_available()
+
+
+def np_now(tensor):
+    return tensor.detach().cpu().numpy()
+
+
+def clamp(x, lo=0, hi=1):
+    return max(lo, min(hi, x))
+
+
+class PruneMask():
+    def __init__(self, layer, prune_rnn_input):
+        self.mask = []
+        self.p_idx = [0]
+        self.total_params = 0
+        self.pruned_params = 0
+        self.split_size = 0
+        self.init_mask(layer, prune_rnn_input)
+
+    def init_mask(self, layer, prune_rnn_input):
+        # Determine the layer type and
+        # num matrix splits if rnn
+        layer_type = str(layer).split('(')[0]
+        splits = {'Linear': 1, 'GRU': 3, 'LSTM': 4}
+
+        # Organise the num and indices of layer parameters
+        # Dense will have one index and rnns two (if pruning input)
+        if layer_type != 'Linear':
+            self.p_idx = [0, 1] if prune_rnn_input else [1]
+
+        # Get list of parameters from layers
+        params = self.get_params(layer)
+
+        # For each param matrix in this layer, create a mask
+        for W in params:
+            self.mask += [torch.ones_like(W)]
+            self.total_params += W.size(0) * W.size(1)
+
+        # Need a split size for mask_from_matrix() later on
+        self.split_size = self.mask[0].size(0) // splits[layer_type]
+
+    def get_params(self, layer):
+        params = []
+        for idx in self.p_idx:
+            params += [list(layer.parameters())[idx].data]
+        return params
+
+    def update_mask(self, layer, z):
+        params = self.get_params(layer)
+        for i, W in enumerate(params):
+            self.mask[i] = self.mask_from_matrix(W, z)
+        self.update_prune_count()
+
+    def apply_mask(self, layer):
+        params = self.get_params(layer)
+        for M, W in zip(self.mask, params): W *= M
+
+    def mask_from_matrix(self, W, z):
+        # Split into gate matrices (or not)
+        W_split = torch.split(W, self.split_size)
+
+        M = []
+        # Loop through splits
+        for W in W_split:
+            # Sort the magnitudes
+            N = W.shape[0]
+            W = np.transpose(W, (1, 0))
+            W_abs = torch.abs(W)
+            L = np.reshape(W_abs, (-1, N // hp.sparse_group, hp.sparse_group))
+            S = L.sum(dim=-1)
+            sorted_abs, _ = torch.sort(S.view(-1))
+
+            # Pick k (num weights to zero)
+            k = int(W.size(0) * W.size(1) // hp.sparse_group * z)
+            threshold = sorted_abs[k]
+            mask = (S >= threshold).float()
+            mask = np.repeat(mask, hp.sparse_group, axis=1)
+            mask = np.transpose(mask, (1, 0))
+            # Create the mask
+            M += [mask]
+
+        return torch.cat(M)
+
+    def update_prune_count(self):
+        self.pruned_params = 0
+        for M in self.mask:
+            self.pruned_params += int(np_now((M - 1).sum() * -1))
+
+
+class Pruner(object):
+    def __init__(self, layers, start_prune, prune_steps, target_sparsity,
+                 prune_rnn_input=True):
+        self.z = 0  # Objects sparsity @ time t
+        self.t_0 = start_prune
+        self.S = prune_steps
+        self.Z = target_sparsity
+        self.num_pruned = 0
+        self.total_params = 0
+        self.masks = []
+        self.layers = layers
+        for layer in layers:
+            self.masks += [PruneMask(layer, prune_rnn_input)]
+        self.count_total_params()
+
+    def update_sparsity(self, t):
+        z = self.Z * (1 - (1 - (t - self.t_0) / self.S) ** 3)
+        self.z = clamp(z, 0, self.Z)
+        return
+
+    def prune(self, step):
+        self.update_sparsity(step)
+        for (l, m) in zip(self.layers, self.masks):
+            m.update_mask(l, self.z)
+            m.apply_mask(l)
+        return self.count_num_pruned()
+
+    def restart(self, layers, step):
+        # In case training is stopped
+        self.update_sparsity(step)
+        for (l, m) in zip(layers, self.masks):
+            m.update_mask(l, self.z)
+
+    def count_num_pruned(self):
+        self.num_pruned = 0
+        for m in self.masks:
+            self.num_pruned += m.pruned_params
+        return self.num_pruned
+
+    def count_total_params(self):
+        for m in self.masks:
+            self.total_params += m.total_params
+
 
 def save_checkpoint(device, model, optimizer, step, checkpoint_dir, epoch):
     checkpoint_path = join(
@@ -95,7 +219,7 @@ def test_save_checkpoint():
     global global_step, global_epoch, global_test_step
     save_checkpoint(device, model, optimizer, global_step, checkpoint_path, global_epoch)
 
-    model = load_checkpoint(checkpoint_path+"checkpoint_step000000000.pth", model, optimizer, False)
+    model = load_checkpoint(checkpoint_path + "checkpoint_step000000000.pth", model, optimizer, False)
 
 
 def evaluate_model(model, data_loader, checkpoint_dir, limit_eval_to=5):
@@ -105,16 +229,16 @@ def evaluate_model(model, data_loader, checkpoint_dir, limit_eval_to=5):
     test_path = data_loader.dataset.test_path
     test_files = os.listdir(test_path)
     counter = 0
-    output_dir = os.path.join(checkpoint_dir,'eval')
+    output_dir = os.path.join(checkpoint_dir, 'eval')
     for f in test_files:
         if f[-7:] == "mel.npy":
-            mel = np.load(os.path.join(test_path,f))
+            mel = np.load(os.path.join(test_path, f))
             wav = model.generate(mel, batched=True)
             # save wav
-            wav_path = os.path.join(output_dir,"checkpoint_step{:09d}_wav_{}.wav".format(global_step,counter))
+            wav_path = os.path.join(output_dir, "checkpoint_step{:09d}_wav_{}.wav".format(global_step, counter))
             librosa.output.write_wav(wav_path, wav, sr=hp.sample_rate)
             # save wav plot
-            fig_path = os.path.join(output_dir,"checkpoint_step{:09d}_wav_{}.png".format(global_step,counter))
+            fig_path = os.path.join(output_dir, "checkpoint_step{:09d}_wav_{}.png".format(global_step, counter))
             fig = plt.plot(wav.reshape(-1))
             plt.savefig(fig_path)
             # clear fig to drawing to the same plot
@@ -123,10 +247,12 @@ def evaluate_model(model, data_loader, checkpoint_dir, limit_eval_to=5):
             if counter == 0:
                 wav = model.generate(mel, batched=False)
                 # save wav
-                wav_path = os.path.join(output_dir,"checkpoint_step{:09d}_wav_unbatched_{}.wav".format(global_step,counter))
+                wav_path = os.path.join(output_dir,
+                                        "checkpoint_step{:09d}_wav_unbatched_{}.wav".format(global_step, counter))
                 librosa.output.write_wav(wav_path, wav, sr=hp.sample_rate)
                 # save wav plot
-                fig_path = os.path.join(output_dir,"checkpoint_step{:09d}_wav_unbatched_{}.png".format(global_step,counter))
+                fig_path = os.path.join(output_dir,
+                                        "checkpoint_step{:09d}_wav_unbatched_{}.png".format(global_step, counter))
                 fig = plt.plot(wav.reshape(-1))
                 plt.savefig(fig_path)
                 # clear fig to drawing to the same plot
@@ -156,7 +282,9 @@ def train_loop(device, model, data_loader, optimizer, checkpoint_dir):
     else:
         raise ValueError("input_type:{} not supported".format(hp.input_type))
 
-    
+    # Pruner for reducing memory footprint
+    layers = [model.rnn1, model.rnn2]
+    pruner = Pruner(layers, hp.start_prune, hp.prune_steps, hp.sparsity_target)
 
     global global_step, global_epoch, global_test_step
     while global_epoch < hp.nepochs:
@@ -170,7 +298,8 @@ def train_loop(device, model, data_loader, optimizer, checkpoint_dir):
             if hp.fix_learning_rate:
                 current_lr = hp.fix_learning_rate
             elif hp.lr_schedule_type == 'step':
-                current_lr = step_learning_rate_decay(hp.initial_learning_rate, global_step, hp.step_gamma, hp.lr_step_interval)
+                current_lr = step_learning_rate_decay(hp.initial_learning_rate, global_step, hp.step_gamma,
+                                                      hp.lr_step_interval)
             else:
                 current_lr = noam_learning_rate_decay(hp.initial_learning_rate, global_step, hp.noam_warm_up_steps)
             for param_group in optimizer.param_groups:
@@ -182,7 +311,7 @@ def train_loop(device, model, data_loader, optimizer, checkpoint_dir):
             optimizer.step()
 
             running_loss += loss.item()
-            avg_loss = running_loss / (i+1)
+            avg_loss = running_loss / (i + 1)
 
             writer.add_scalar("loss", float(loss.item()), global_step)
             writer.add_scalar("avg_loss", float(avg_loss), global_step)
@@ -191,9 +320,11 @@ def train_loop(device, model, data_loader, optimizer, checkpoint_dir):
 
             # saving checkpoint if needed
             if global_step != 0 and global_step % hp.save_every_step == 0:
+                pruner.prune(global_step)
                 save_checkpoint(device, model, optimizer, global_step, checkpoint_dir, global_epoch)
             # evaluate model if needed
-            if global_step != 0 and global_test_step !=True and global_step % hp.evaluate_every_step == 0:
+            if global_step != 0 and global_test_step != True and global_step % hp.evaluate_every_step == 0:
+                pruner.prune(global_step)
                 print("step {}, evaluating model: generating wav from mel...".format(global_step))
                 evaluate_model(model, data_loader, checkpoint_dir)
                 print("evaluation finished, resuming training...")
@@ -202,15 +333,30 @@ def train_loop(device, model, data_loader, optimizer, checkpoint_dir):
             if global_test_step is True:
                 global_test_step = False
             global_step += 1
-        
-        print("epoch:{}, running loss:{}, average loss:{}, current lr:{}".format(global_epoch, running_loss, avg_loss, current_lr))
+
+        pruner.prune(global_step)
+        print("epoch:{}, running loss:{}, average loss:{}, current lr:{}".format(global_epoch, running_loss, avg_loss,
+                                                                                 current_lr))
         global_epoch += 1
 
 
+def test_prune(model):
+    layers = [model.rnn1, model.rnn2]
+    start_prune = 0
+    prune_steps = 100  # 20000
+    sparsity_target = 0.9375
+    pruner = Pruner(layers, start_prune, prune_steps, sparsity_target)
 
-if __name__=="__main__":
+    for i in range(100):
+        n_pruned = pruner.prune(100)
+        print(f'{i}: {n_pruned}')
+
+    return layers
+
+
+if __name__ == "__main__":
     args = docopt(__doc__)
-    #print("Command line args:\n", args)
+    # print("Command line args:\n", args)
     checkpoint_dir = args["--checkpoint-dir"]
     checkpoint_path = args["--checkpoint"]
     data_root = args["<data-root>"]
@@ -218,7 +364,7 @@ if __name__=="__main__":
 
     # make dirs, load dataloader and set up device
     os.makedirs(checkpoint_dir, exist_ok=True)
-    os.makedirs(os.path.join(checkpoint_dir,'eval'), exist_ok=True)
+    os.makedirs(os.path.join(checkpoint_dir, 'eval'), exist_ok=True)
     dataset = AudiobookDataset(data_root)
     if hp.input_type == 'raw':
         collate_fn = raw_collate
@@ -228,7 +374,8 @@ if __name__=="__main__":
         collate_fn = discrete_collate
     else:
         raise ValueError("input_type:{} not supported".format(hp.input_type))
-    data_loader = DataLoader(dataset, collate_fn=collate_fn, shuffle=True, num_workers=int(hp.num_workers), batch_size=hp.batch_size)
+    data_loader = DataLoader(dataset, collate_fn=collate_fn, shuffle=True, num_workers=int(hp.num_workers),
+                             batch_size=hp.batch_size)
     device = torch.device("cuda" if use_cuda else "cpu")
     print("using device:{}".format(device))
 
@@ -242,20 +389,20 @@ if __name__=="__main__":
     # build model, create optimizer
     model = build_model().to(device)
     print("Parameter Count:")
-    print("I: %.3f million"%(num_params_count(model.I)))
-    print("Upsample: %.3f million"%(num_params_count(model.upsample)))
-    print("rnn1: %.3f million"%(num_params_count(model.rnn1)))
-    print("rnn2: %.3f million"%(num_params_count(model.rnn2)))
-    print("fc1: %.3f million"%(num_params_count(model.fc1)))
-    print("fc2: %.3f million"%(num_params_count(model.fc2)))
-    print("fc3: %.3f million"%(num_params_count(model.fc3)))
+    print("I: %.3f million" % (num_params_count(model.I)))
+    print("Upsample: %.3f million" % (num_params_count(model.upsample)))
+    print("rnn1: %.3f million" % (num_params_count(model.rnn1)))
+    print("rnn2: %.3f million" % (num_params_count(model.rnn2)))
+    print("fc1: %.3f million" % (num_params_count(model.fc1)))
+    print("fc2: %.3f million" % (num_params_count(model.fc2)))
+    print("fc3: %.3f million" % (num_params_count(model.fc3)))
     print(model)
 
     optimizer = optim.Adam(model.parameters(),
                            lr=hp.initial_learning_rate, betas=(
-        hp.adam_beta1, hp.adam_beta2),
-        eps=hp.adam_eps, weight_decay=hp.weight_decay,
-        amsgrad=hp.amsgrad)
+            hp.adam_beta1, hp.adam_beta2),
+                           eps=hp.adam_eps, weight_decay=hp.weight_decay,
+                           amsgrad=hp.amsgrad)
 
     if hp.fix_learning_rate:
         print("using fixed learning rate of :{}".format(hp.fix_learning_rate))
@@ -272,6 +419,7 @@ if __name__=="__main__":
         print("loading model from checkpoint:{}".format(checkpoint_path))
         # set global_test_step to True so we don't evaluate right when we load in the model
         global_test_step = True
+        hp.start_prune = 0 #start pruning right away if loaded from checkpoint
 
     # main train loop
     try:
@@ -282,7 +430,7 @@ if __name__=="__main__":
     finally:
         print("saving model....")
         save_checkpoint(device, model, optimizer, global_step, checkpoint_dir, global_epoch)
-    
+
 
 def test_eval():
     data_root = "data_dir"
@@ -301,8 +449,3 @@ def test_eval():
     model = build_model().to(device)
 
     evaluate_model(model, data_loader)
-
-    
-
-    
-
