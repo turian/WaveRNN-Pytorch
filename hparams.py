84c43f8992793ed6c6b9fc5d0461cd11d31d558f
geneing <geneing@gmail.com>
2019-01-22 23:22:08 -0800

    Smaller resnet. Single GRU layer.

--- hparams.py
+++ hparams.py
@@ -56,7 +56,7 @@ hparams = tf.contrib.training.HParams(
     #
     # ----------------
     # training parameters
-    batch_size=32,
+    batch_size=16,
     nepochs=5000,
     save_every_step=5000,
     evaluate_every_step=5000,
