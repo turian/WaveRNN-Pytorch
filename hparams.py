2141b7b086f80fae3f2d3ab56f12b58077867246
geneing <geneing@gmail.com>
2019-01-23 10:12:02 -0800

    Merge branch 'experiment_2'
    # Conflicts:
    #	Description.md
    #	hparams.py

--- hparams.py
+++ hparams.py
@@ -56,7 +56,7 @@ hparams = tf.contrib.training.HParams(
     #
     # ----------------
     # training parameters
-    batch_size=16,
+    batch_size=32,
     nepochs=5000,
     save_every_step=5000,
     evaluate_every_step=5000,
