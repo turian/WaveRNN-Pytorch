6c75c5b8897e4e6ede912d7f31ac29dfc0b4294a
geneing <geneing@gmail.com>
2019-01-29 08:06:10 -0800

    WIP

--- library/src/wavernn.h
+++ library/src/wavernn.h
@@ -7,19 +7,22 @@
 
 using namespace Eigen;
 
-typedef Matrix<float, Dynamic, Dynamic, RowMajor> MatrixXf;
-typedef Tensor<float, 3, RowMajor> TensorXf;
+typedef Matrix<float, Dynamic, Dynamic, RowMajor> Matrixf;
+typedef Tensor<float, 3, RowMajor> Tensorf;
+typedef VectorXf Vectorf;
 
 
 class TorchLayer{
     struct Header{
         int size; //size of data blob, not including this header
         enum LayerType : char { Conv1d=1, BatchNorm1d=2, Linear=3, GRU=4 };
+        char elSize;  //size of each entry in bytes: 4 for float, 2 for fp16.
+        char name[64]; //layer name for debugging
     };
 
 public:
     TorchLayer* loadNext( FILE* fd );
-    virtual VectorXf operator()( VectorXf& x ) = 0;
+    virtual Vectorf operator()( Vectorf& x ) = 0;
     virtual ~TorchLayer() = default;
 };
 
@@ -32,10 +35,8 @@ class Conv1d : TorchLayer{
         int kernelSize;
     };
 
-
-
-    TensorXf weight;
-    VectorXf bias;
+    Tensorf weight;
+    Vectorf bias;
 
 
     //call TorchLayer loadNext, not derived loadNext
@@ -43,7 +44,7 @@ class Conv1d : TorchLayer{
 
 public:
 
-    virtual VectorXf operator()( VectorXf& x );
+    virtual Vectorf operator()( Vectorf& x );
 
 };
 
@@ -54,22 +55,60 @@ class BatchNorm1d : TorchLayer{
     };
 
 
-    VectorXf weight;
-    VectorXf bias;
-    VectorXf running_mean;
-    VectorXf running_var;
+    Vectorf weight;
+    Vectorf bias;
+    Vectorf running_mean;
+    Vectorf running_var;
 
     //call TorchLayer loadNext, not derived loadNext
     BatchNorm1d* loadNext( FILE* fd );
 
 public:
 
-    virtual VectorXf operator()( VectorXf& x );
+    virtual Vectorf operator()( Vectorf& x );
 
 };
 
 
+class Linear : TorchLayer{
+    struct Header{
+        int inChannels;
+        int outChannels;
+    };
 
+    Matrixf weight;
+    Vectorf bias;
+
+    //call TorchLayer loadNext, not derived loadNext
+    Linear* loadNext( FILE* fd );
+
+public:
+
+    virtual Vectorf operator()( Vectorf& x );
+
+};
+
+
+class GRU : TorchLayer{
+    struct Header{
+        int inChannels;
+        int outChannels;
+        int kernelSize;
+    };
+
+    Matrixf W_ir,W_iz,W_in;
+    Matrixf W_hr,W_hz,W_hn;
+    Vectorf b_ir,b_iz,b_in;
+    Vectorf b_hr,b_hz,b_hn;
+
+    //call TorchLayer loadNext, not derived loadNext
+    Linear* loadNext( FILE* fd );
+
+public:
+
+    virtual Vectorf operator()( Vectorf& x, Vectorf& hx );
+
+};
 
 
 #endif // WAVERNN_H
